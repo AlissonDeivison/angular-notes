a2aa366c3f5cfba4afe723a1231edc6141b3d107
Alisson Deivison S Pereira <uepbalisson@gmail.com>
2024-03-06 16:51:31 -0300

    Adiciona rotas para criar novo pensamento e listar pensamentos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateNewThoughtComponent } from './components/thoughts/create-new-thought/create-new-thought.component';
+import { ThoughtsListComponent } from './components/thoughts/thoughts-list/thoughts-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'createNewThought',
+    pathMatch: 'full'
+  },
+  {
+    path: 'createNewThought',
+    component: CreateNewThoughtComponent
+  },
+  {
+    path: 'thought-list',
+    component: ThoughtsListComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
