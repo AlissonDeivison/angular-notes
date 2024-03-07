44e64e219a1285cca960a4a53e36fbb81e169e55
Alisson Deivison S Pereira <uepbalisson@gmail.com>
2024-03-07 06:00:36 -0300

    Adicionado decorator de Input para receber o objeto thought do componente pai. Atualizado o valor da propriedade model para 'modelo2'.

--- src/app/components/thoughts/thought/thought.component.ts
+++ src/app/components/thoughts/thought/thought.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-thought',
@@ -6,9 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './thought.component.scss'
 })
 export class ThoughtComponent {
-  thought = {
+  //Input decorator to receive the thought object from the parent component. This component calls dumb component, because it doesn't have any complex logic, it just receives data and displays it
+  @Input() thought = {
     content: 'You are what you think',
     author: 'Unknown',
-    model: 'modelo3'
+    model: 'modelo2'
   }
 }
