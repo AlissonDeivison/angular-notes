bdd993bc8391bc0c34f6495277fcbc701e2bdb42
Alisson Deivison S Pereira <uepbalisson@gmail.com>
2024-03-08 07:19:23 -0300

    Atualizar o componente de pensamento para adicionar um link de roteamento para excluir um pensamento específico.

--- src/app/components/thoughts/thought/thought.component.ts
+++ src/app/components/thoughts/thought/thought.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Thought } from '../thought.type';
+import { ThoughtService } from '../thought.service';
 
 @Component({
   selector: 'app-thought',
   templateUrl: './thought.component.html',
   styleUrl: './thought.component.scss'
 })
+
 export class ThoughtComponent {
   //Input decorator to receive the thought object from the parent component. This component calls dumb component, because it doesn't have any complex logic, it just receives data and displays it
   @Input() thought : Thought = {
